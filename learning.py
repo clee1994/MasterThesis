98670d45c87c23ff3148dfb9857ae1631250f946
clee1994 <clemensschaefer1994@gmail.com>
2017-06-18 16:03:27 +0100

    Flie structure cchange

--- learning.py
+++ learning.py
@@ -1,17 +1,13 @@
-import tensorflow as tf
-import numpy as np
 
-f = open('./Data/processed_data', 'rb')
-[prices, dates, names, lreturns, news_data, faulty_news] = pickle.load(f)
-f.close()
+def learning_news_effect(alone, prices=0, dates=0, names=0, lreturns=0, 
+	news_data=0)
 
-#imporve with news data
+	import tensorflow as tf
+	import numpy as np
 
+	if alone:
+		f = open('./Data/processed_data', 'rb')
+		[prices, dates, names, lreturns, news_data, faulty_news] = pickle.load(f)
+		f.close()
 
-#minimum variance portfolio projected CG
-
-
-#sparsity minimum variance
-
-
-#performance testing
+	
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from data_loading import load_data
+from learning import learning_news_effect
+
 
 #load and preprocess data
-
+[prices, dates, names, lreturns, news_data, faulty_news] = load_data(True)
 
 #learn news effect
+
 
 
 #minimum variance portfolio projected CG
