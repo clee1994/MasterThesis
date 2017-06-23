4b3e73bb06466d226988affe3f6531cfe7e45946
clee1994 <clemensschaefer1994@gmail.com>
2017-06-23 10:19:25 +0100

    change in progressbar char

--- progressbar.py
+++ progressbar.py
@@ -1,6 +1,6 @@
 #source: https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '='):
     """
     Call in a loop to create terminal progress bar
     @params:
