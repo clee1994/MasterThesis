d692d30fa0212c8817358a4d918f06e03d28f1b2
clee1994 <clemensschaefer1994@gmail.com>
2017-07-27 15:41:16 +0100

    Perfromance Table #1

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ del news_data, x_fts, x_ws, x_mc
 gc.collect()
 
 
-pickle.dump((mu_p_ts,cov_p_ts,firm_ind_u), open( "data_mu_cov.p", "wb" ) )
+pickle.dump((mu_p_ts,cov_p_ts,firm_ind_u), open( "Output/data_mu_cov.p", "wb" ) )
 
 # 6. standard past observation past mu and cov
 split_point = int(np.floor(np.shape(x_dates)[0]*(1-test_split)))
@@ -131,7 +131,7 @@ final_plots([first_line,second_line,third_line,sp500],[r'min. var. portfolio (pa
 #return plots
 final_plots_s([r1,r2,r3,r4],[r'past obs.', r'doc2vec',r'doc2vec, l1',r'SP500'])
 
-pickle.dump((first_line,second_line,third_line,sp500,r1,r2,r3,r4), open( "datal.p", "wb" ) )
+pickle.dump((first_line,second_line,third_line,sp500,r1,r2,r3,r4), open( "Output/datal.p", "wb" ) )
 
 
 f = open('Output/tables/'+str(datetime.datetime.now())+'performance.tex', 'w')
