8e32542e89b37e346cd56ccd9862cbe1576c1457
clee1994 <clemensschaefer1994@gmail.com>
2017-08-05 15:17:07 +0100

    AWS

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def main_x_reg(x_method):
 	else:
 		# doc2vec
 		[x_gram, dates_news] = learning.calibrate_doc2vec(np.reshape(lreturns[:,firm_ind_u[0]], (np.shape(lreturns)[0],1)),dates_prices,test_split,news_data)
+		tfidf = []
 		del news_data
 		print(str(datetime.datetime.now())+': Successfully doc2vec')
 		gc.collect()
