a109f90b16ee36800e8f6510169b143a63652d9e
clee1994 <clemensschaefer1994@gmail.com>
2017-08-20 12:54:27 +0200

    AWS regression parameters refined

--- learning.py
+++ learning.py
@@ -89,7 +89,6 @@ def create_documents(news,lreturns,dates_stocks,ylm):
 		pass
 
 	print('] Done', flush=True)
-	import pdb; pdb.set_trace()  # breakpoint 111ce945 //
 
 	y_ret =  np.array(y)
 	print('cr_test1')
